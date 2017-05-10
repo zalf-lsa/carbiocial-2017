05e3ad192bdba44e005f73fc587e71134c6a0540
TStella <tommaso.stella@zalf.de>
2017-05-10 16:45:35 +0200

    slow producer
    working... slowly

--- relative_to_abs_ws.py
+++ relative_to_abs_ws.py
@@ -1,11 +1,10 @@
 import os
 import csv
-from datetime import date
-from datetime import timedelta
+from datetime import date, timedelta
 import json
 import copy
 
-def generate_abs_rotation(rel_rotation, start_year, end_year):#, ref_dates):
+def generate_abs_rotation(rel_rotation, start_year, end_year, ref_dates):
     "generate absolute dates rotation based on rain season onset"    
 
     with open("all_crops.json") as all_crops:
@@ -20,7 +19,7 @@ def generate_abs_rotation(rel_rotation, start_year, end_year):#, ref_dates):
             next_index = index + 1
         return next_crp, next_index
 
-    def create_cultivation_method(year, crop_info):
+    def create_cultivation_method(year, crop_info, crop_id, sowing_soy, early_harvest_soy):
         cultivation_method = {"worksteps": []}
         added_year = False
         for step in crop_info["worksteps"]:
@@ -28,10 +27,18 @@ def generate_abs_rotation(rel_rotation, start_year, end_year):#, ref_dates):
             for k,v in mystep.iteritems():
                 if "0000-" in str(v):
                     mystep[k] = v.replace("0000", str(year))
+                    if crop_id == "soybean" and mystep["type"] == "AutomaticSowing":
+                        earliest_sowing = date(year, 1, 1) + timedelta(days = sowing_soy - 1)
+                        mystep[k] = mystep[k].replace("09", str.zfill(str(earliest_sowing.month), 2))
+                        mystep[k] = mystep[k].replace("15", str.zfill(str(earliest_sowing.day), 2))
+                        mystep["latest-date"] = unicode((earliest_sowing + timedelta(days = 30)).isoformat())
                 if "0001-" in str(v):
                     if not added_year: year +=1
                     added_year = True
+                    if crop_id == "soybean" and mystep["type"] == "AutomaticHarvest" and early_harvest_soy:
+                        v = v.replace("03-01", "02-10")
                     mystep[k] = v.replace("0001", str(year))
+                    
             cultivation_method["worksteps"].append(mystep)
         return year, cultivation_method
 
@@ -39,17 +46,29 @@ def generate_abs_rotation(rel_rotation, start_year, end_year):#, ref_dates):
     current_index = -1 #identifies current crop
     
     rotation_abs_dates = []
+
+    early_harvest_soy = False
+    if "cotton" in rel_rotation:
+        early_harvest_soy = True
     
     while current_year <= end_year:
 
         crop_in_rotation, current_index = next_crop(rel_rotation, current_index)
         
-        current_year, cultivation_method = create_cultivation_method(current_year, all_crops[crop_in_rotation])
+        current_year, cultivation_method = create_cultivation_method(current_year, all_crops[crop_in_rotation], crop_in_rotation, ref_dates[current_year], early_harvest_soy)
 
         rotation_abs_dates.append(cultivation_method)
+    
+    #with open("test_abs_rot.json", "w") as _:
+    #    _.write(json.dumps(rotation_abs_dates))
 
     return rotation_abs_dates
 
-rotation = ["soybean", "maize"]
+#rotation = ["soybean", "cotton"]
+#ref_dates = {}
+#doy = 244
+#for i in range(1980, 2011):
+#    ref_dates[i] = doy
+#    doy += 1 
 
-generate_abs_rotation(rotation, 1980, 2010)
+#generate_abs_rotation(rotation, 1980, 2010, ref_dates)
--- run-work-producer.py
+++ run-work-producer.py
@@ -15,40 +15,34 @@
 # Landscape Systems Analysis at the ZALF.
 # Copyright (C: Leibniz Centre for Agricultural Landscape Research (ZALF)
 
-#import copy
 import csv
 import json
-#import os
+import os
 import sqlite3
-#import types
-
 import sys
-
 import time
-
 import zmq
-
-#from soil_conversion import *
-#import monica_python
 import monica_io
 import soil_io
 import ascii_io
 from datetime import date, timedelta
 import copy
+from relative_to_abs_ws import generate_abs_rotation
+import numpy as np
 
-#print "pyzmq version: ", zmq.pyzmq_version()
-#print "sys.path: ", sys.path
-#print "sys.version: ", sys.version
+
 USER = "stella"
 
 PATHS = {
     "stella": {
         "INCLUDE_FILE_BASE_PATH": "C:/Users/stella/Documents/GitHub",
-        "LOCAL_PATH_TO_ARCHIV": "Z:/projects/carbiocial"
+        "LOCAL_PATH_TO_ARCHIV": "Z:/projects/carbiocial/",
+        "LOCAL_PATH_TO_REPO": "C:/Users/stella/Documents/GitHub/carbiocial-2017/"
     }
 }
 
 PATH_TO_ARCHIV_DIR = "/archiv-daten/md/projects/carbiocial/"
+start_send = time.clock()
 
 def main():
     "main function"
@@ -58,7 +52,7 @@ def main():
     port = 6666 if len(sys.argv) == 1 else sys.argv[1]
     socket.connect("tcp://cluster2:" + str(port))
 
-    soil_db_con = sqlite3.connect(PATHS[USER]["LOCAL_PATH_TO_ARCHIV"] + "carbiocial.sqlite")
+    soil_db_con = sqlite3.connect(PATHS[USER]["LOCAL_PATH_TO_REPO"] + "soil-carbiocial.sqlite")
 
     with open("sim.json") as _:
         sim = json.load(_)
@@ -72,231 +66,124 @@ def main():
     with open("all_crops.json") as _:
         all_crops = json.load(_)
 
-    sim["include-file-base-path"] = PATHS[USER]["INCLUDE_FILE_BASE_PATH"]    
+    sim["include-file-base-path"] = PATHS[USER]["INCLUDE_FILE_BASE_PATH"]
 
-    def read_general_metadata(path_to_file):
-        "read metadata file"
+    periods = [
+        {
+            "name": "historical",
+            "start_year": 1981,
+            "end_year": 1984, #TODO 2012
+            "climate_folder": "climate-data-years-1981-2012-rows-0-2544"
+        },
+        {
+            "name": "future_wrf",
+            "start_year": 2013,
+            "end_year": 2040,
+            "climate_folder": "climate-data-years-2001-2040-rows-0-2544"
+        },
+        {
+            "name": "future_starr",
+            "start_year": 2013,
+            "end_year": 2040,
+            "climate_folder": "climate-data-years-2013-2040-rows-0-2544"
+        }
+    ]
+
+    rotations = [
+        ["soybean", "cotton"],
+        ["soybean", "maize"]
+    ]
+
+    n_rows = 2544
+    n_cols = 1927
+
+    def ascii_grid_to_2darray(path_to_file):
+        "0=row, 1=col"
         with open(path_to_file) as file_:
-            data = {}
-            reader = csv.reader(file_, delimiter="\t")
-            reader.next()
-            for row in reader:
-                if int(row[1]) != 1:
-                    continue
-                data[(int(row[2]), int(row[3]))] = {
-                    "subpath-climate.csv": row[9],
-                    "latitude": float(row[13]),
-                    "elevation": float(row[14])
-                }
-            return data
-
-    general_metadata = read_general_metadata("NRW_General_Metadata.csv")
-
+            for header in range(0, 6):
+                file_.next()
+            out = []
+            for line in file_:
+                row = line.split(' ')
+                out.append(row)
+            return out
     
-    def load_mapping(row_offset=0, col_offset=0):
-        to_climate_index = {}
-        with(open("working_resolution_to_climate_lat_lon_indices.json")) as _:
-            l = json.load(_)
-            for i in xrange(0, len(l), 2):
-                cell = (row_offset + l[i][0], col_offset + l[i][1])
-                to_climate_index[cell] = tuple(l[i+1])
-            return to_climate_index
+    def grids_to_dictof2darrays(path_to_grids):
+        "key=year, 0=row, 1=col"
+        out = {}
+        for filename in os.listdir(path_to_grids):
+            year = int(filename.split(".")[0])
+            out[year] = ascii_grid_to_2darray(path_to_grids + "/" + filename)
+        return out
+    
+    def update_soil(row, col):
+        "in place update the env"        
+        def lat(con, profile_id):
+            query = """
+            select                
+                lat_times_1000
+            from soil_profile_data 
+            where id = ? 
+            order by id
+            """
+            con.row_factory = sqlite3.Row
+            for row in con.cursor().execute(query, (profile_id,)):
+                if row["lat_times_1000"]:
+                    return float(row["lat_times_1000"])
 
-    def read_orgN_kreise(path_to_file):
-        "read organic N info for kreise"
-        with open(path_to_file) as file_:
-            data = {}
-            reader = csv.reader(file_, delimiter=",")
-            reader.next()
-            reader.next()
-            for row in reader:
-                for kreis_code in row[1].split("|"):
-                    if kreis_code != "":
-                        data[int(kreis_code)] = float(row[8])
-        return data
-
-    orgN_kreise = read_orgN_kreise("NRW_orgN_balance.csv")
-
-    def update_soil_crop_dates(row, col):
-        "in place update the env"
-        #startDate = date(1980, 1, 1)# + timedelta(days = p["sowing-doy"])
-        #sim["start-date"] = startDate.isoformat()
-        #sim["end-date"] = date(2010, 12, 31).isoformat()
-        #sim["debug?"] = True
-
-        site["Latitude"] = general_metadata[(row, col)]["latitude"]
-        site["HeightNN"] = [general_metadata[(row, col)]["elevation"], "m"]
-        site["SiteParameters"]["SoilProfileParameters"] = soil_io.soil_parameters(soil_db_con, soil_ids[(row, col)])
+        site["Latitude"] = lat(soil_db_con, soil_ids[row][col])
+        #site["HeightNN"] = #TODO
+        site["SiteParameters"]["SoilProfileParameters"] = soil_io.soil_parameters(soil_db_con, soil_ids[row][col])
         for layer in site["SiteParameters"]["SoilProfileParameters"]:
             layer["SoilBulkDensity"][0] = max(layer["SoilBulkDensity"][0], 600)
-    
-    def read_ascii_grid(path_to_file, include_no_data=False, row_offset=0, col_offset=0):
-        "read an ascii grid into a map, without the no-data values"
-        def int_or_float(s):
-            try:
-                return int(s)
-            except ValueError:
-                return float(s)
-        
-        with open(path_to_file) as file_:
-            data = {}
-            #skip the header (first 6 lines)
-            for _ in range(0, 6):
-                file_.next()
-            row = 0
-            for line in file_:
-                col = 0
-                for col_str in line.strip().split(" "):
-                    if not include_no_data and int_or_float(col_str) == -9999:
-                        continue
-                    data[(row_offset+row, col_offset+col)] = int_or_float(col_str)
-                    col += 1
-                row += 1
-            return data
-    
-    #offset is used to match info in general metadata and soil database
-    soil_ids = read_ascii_grid("soil-profile-id_nrw_gk3.asc", row_offset=282)
-    bkr_ids = read_ascii_grid("bkr_nrw_gk3.asc", row_offset=282)
-    lu_ids = read_ascii_grid("lu_resampled.asc", row_offset=282)
-    kreise_ids = read_ascii_grid("kreise_matrix.asc", row_offset=282)
-    meteo_ids = load_mapping(row_offset=282)
 
-    def rotate(crop_rotation):
-        "rotate the crops in the rotation"
-        crop_rotation.insert(0, crop_rotation.pop())
-    
-    def insert_cc(crop_rotation):
-        "insert cover crops in the rotation"
-        insert_cover_before = ["maize", "spring barley", "potato", "sugar beet"]
-        insert_cover_here = []
-        for cultivation_method in range(len(crop_rotation)):
-            for workstep in crop_rotation[cultivation_method]["worksteps"]:
-                if workstep["type"] == "Sowing":
-                    if workstep["crop"]["cropParams"]["species"]["SpeciesName"] in insert_cover_before or workstep["crop"]["cropParams"]["cultivar"]["CultivarName"] in insert_cover_before:
-                        insert_cover_here.append((cultivation_method, workstep["date"]))
-                        break
-        for position, mydate in reversed(insert_cover_here): 
-            mydate = mydate.split("-")
-            main_crop_sowing = date(2017, int(mydate[1]), int(mydate[2]))
-            latest_harvest_cc = main_crop_sowing - timedelta(days = 10)
-            latest_harvest_cc = unicode("0001-" + str(latest_harvest_cc.month).zfill(2) + "-" + str(latest_harvest_cc.day).zfill(2))
-            crop_rotation.insert(position, copy.deepcopy(cc_data))
-            crop_rotation[position]["worksteps"][1]["latest-date"] = latest_harvest_cc
+    soil_ids = ascii_grid_to_2darray(PATHS[USER]["LOCAL_PATH_TO_ARCHIV"] + "Soil/Carbiocial_Soil_Raster_final.asc")    
 
-    #def remove_cc(crop_rotation):
-    #    "remove cover crops from the rotation"
-    #    for cultivation_method in reversed(range(len(crop_rotation))):
-    #        if "is-cover-crop" in crop_rotation[cultivation_method]:
-    #            del crop_rotation[cultivation_method]
+    i= 0
 
-    #env built only to have structured data for cover crop
-    cover_env = monica_io.create_env_json_from_json_config({
-        "crop": cover_crop,
-        "site": site,
-        "sim": sim,
-        "climate": ""
-        })
-    cc_data = cover_env["cropRotation"][0]
+    for p in periods:
+        rain_onset = grids_to_dictof2darrays(PATHS[USER]["LOCAL_PATH_TO_ARCHIV"] + "rain_onset_grids/" + p["name"])
+        for row in range(n_rows):
+            for col in range(n_cols):
+                if soil_ids[row][col] == "-9999":
+                    continue
+                #update soil data
+                update_soil(row, col)
+                
+                #produce rotation
+                ref_dates = {}
+                for year in range(p["start_year"], p["end_year"] + 1):
+                    ref_dates[year] = int(rain_onset[year][row][col])
+                for rot in rotations:
+                    abs_rot = generate_abs_rotation(rot, p["start_year"], p["end_year"], ref_dates)
+                    crop["cropRotation"] = abs_rot
 
-    i = 0
-    start_send = time.clock()
+                    #create - send env
+                    env = monica_io.create_env_json_from_json_config({
+                        "crop": crop,
+                        "site": site,
+                        "sim": sim,
+                        "climate": ""
+                    })
 
-    def calculate_orgfert_amount(N_applied, fert_type):
-        "convert N applied in amount of fresh org fert"
-        AOM_DryMatterContent = fert_type["AOM_DryMatterContent"][0]
-        AOM_NH4Content = fert_type["AOM_NH4Content"][0]
-        AOM_NO3Content = fert_type["AOM_NO3Content"][0]
-        CN_Ratio_AOM_Fast = fert_type["CN_Ratio_AOM_Fast"][0]
-        CN_Ratio_AOM_Slow = fert_type["CN_Ratio_AOM_Slow"][0]
-        PartAOM_to_AOM_Fast = fert_type["PartAOM_to_AOM_Fast"][0]
-        PartAOM_to_AOM_Slow = fert_type["PartAOM_to_AOM_Slow"][0]
-        AOM_to_C = 0.45
+                    #set climate file - read by the server
+                    env["csvViaHeaderOptions"] = sim["climate.csv-options"]
+                    env["csvViaHeaderOptions"]["start-date"] = sim["start-date"]
+                    env["csvViaHeaderOptions"]["end-date"] = sim["end-date"]
+                    env["pathToClimateCSV"] = PATH_TO_ARCHIV_DIR + "row-" + str(row) + "/col-" + str(col) + ".csv"
+                    
+                    rot_id = rot[0] + "_" + rot[1]
+                    env["customId"] = p["name"] \
+                                        + "|" + str(row) \
+                                        + "|" + str(col) \
+                                        + "|" + rot_id
 
-        AOM_fast_factor = 1/(CN_Ratio_AOM_Fast/(AOM_to_C * PartAOM_to_AOM_Fast))
-        AOM_slow_factor = 1/(CN_Ratio_AOM_Slow/(AOM_to_C * PartAOM_to_AOM_Slow))
-
-        conversion_coeff = AOM_NH4Content + AOM_NO3Content + AOM_fast_factor + AOM_slow_factor
-        
-        AOM_dry = N_applied / conversion_coeff
-        AOM_fresh = AOM_dry / AOM_DryMatterContent
-
-        return AOM_fresh
-
-    for (row, col), gmd in general_metadata.iteritems():
-
-        if (row, col) in soil_ids and (row, col) in bkr_ids and (row, col) in lu_ids:
-            update_soil_crop_dates(row, col)
-
-            bkr_id = bkr_ids[(row, col)]
-            soil_id = soil_ids[(row, col)]
-            kreis_id = kreise_ids[(row, col)]
-            meteo_id = meteo_ids[(row, col)]
-
-            if bkr_id != 129:
-                continue
-
-            for rot_id, rotation in rotations[str(bkr_id)].iteritems():
-
-                crop["cropRotation"] = rotation
-
-                env = monica_io.create_env_json_from_json_config({
-                    "crop": crop,
-                    "site": site,
-                    "sim": sim,
-                    "climate": ""
-                })
-
-                #with open("test_crop.json", "w") as _:
-                #    _.write(json.dumps(crop))
-
-                #with open("test_site.json", "w") as _:
-                #    _.write(json.dumps(site))
-
-                #with open("test_sim.json", "w") as _:
-                #    _.write(json.dumps(sim))
-
-                insert_cc(env["cropRotation"])
-
-                #assign amount of organic fertilizer
-                for cultivation_method in env["cropRotation"]:
-                    for workstep in cultivation_method["worksteps"]:
-                        if workstep["type"] == "OrganicFertilization":
-                            workstep["amount"] = calculate_orgfert_amount(orgN_kreise[kreis_id], workstep["parameters"])
-
-                #climate is read by the server
-                env["csvViaHeaderOptions"] = sim["climate.csv-options"]
-                env["csvViaHeaderOptions"]["start-date"] = sim["start-date"]
-                env["csvViaHeaderOptions"]["end-date"] = sim["end-date"]
-                env["pathToClimateCSV"] = PATH_TO_CLIMATE_DATA_DIR + "row-" + str(meteo_id[0]) + "/col-" + str(meteo_id[1]) + ".csv"
-                #env["pathToClimateCSV"] = PATH_TO_CLIMATE_DATA_DIR + gmd["subpath-climate.csv"]
-
-                for sim_id, sim_ in sims.iteritems():
-                    if sim_id != "WL.NL.rain":
-                        continue
-                    #sim_id, sim_ = ("potential", sims["potential"])
-                    env["events"] = sim_["output"]
-                    env["params"]["simulationParameters"]["NitrogenResponseOn"] = sim_["NitrogenResponseOn"]
-                    env["params"]["simulationParameters"]["WaterDeficitResponseOn"] = sim_["WaterDeficitResponseOn"]
-                    env["params"]["simulationParameters"]["UseAutomaticIrrigation"] = sim_["UseAutomaticIrrigation"]
-                    env["params"]["simulationParameters"]["UseNMinMineralFertilisingMethod"] = sim_["UseNMinMineralFertilisingMethod"]
-
-                    for rot in range(0, len(env["cropRotation"])):
-                        env["customId"] = rot_id \
-                                        + "|" + sim_id \
-                                        + "|" + str(soil_id) \
-                                        + "|(" + str(row) + "/" + str(col) + ")" \
-                                        + "|" + str(bkr_id) \
-                                        + "|" + str(rot)
-                        socket.send_json(env) 
-                        print "sent env ", i, " customId: ", env["customId"]
-                        i += 1                        
-                        rotate(env["cropRotation"]) 
-
+                    socket.send_json(env) 
+                    print "sent env ", i, " customId: ", env["customId"]
+                    i += 1
 
     stop_send = time.clock()
 
     print "sending ", i, " envs took ", (stop_send - start_send), " seconds"
-
-
 
 main()
